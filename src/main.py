e911d5e67374ea6c9816170a9c6e1f83a1ee663c
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-21 22:16:15 -0300

    Wrap cnpj_lookup call in try-except block for error handling

--- src/main.py
+++ src/main.py
@@ -1,25 +1,29 @@
 from apis.customer_vendor import create_new_customer_vendor
 from apis.receitaws import cnpj_lookup
 
-resp = cnpj_lookup(codcoligada="5", codcfo="F99999", cnpj="07.150.434/0001-17", ie="256016631")
+try:
+    resp = cnpj_lookup(codcoligada="5", codcfo="F99999", cnpj="07.150.434/0001-17", ie="256016631")
 
-create_new_customer_vendor(
-    companyId=resp["companyId"],
-    code=resp["code"],
-    shortName=resp["shortName"],
-    name=resp["name"],
-    type=resp["type"],
-    mainNIF=resp["mainNIF"],
-    stateRegister=resp["stateRegister"],
-    zipCode=resp["zipCode"],
-    streetType=resp["streetType"],
-    streetName=resp["streetName"],
-    number=resp["number"],
-    districtType=resp["districtType"],
-    district=resp["district"],
-    stateCode=resp["stateCode"],
-    cityInternalId=resp["cityInternalId"],
-    phoneNumber=resp["phoneNumber"],
-    email=resp["email"],
-    contributor=resp["contributor"]
-)
+    create_new_customer_vendor(
+        companyId=resp["companyId"],
+        code=resp["code"],
+        shortName=resp["shortName"],
+        name=resp["name"],
+        type=resp["type"],
+        mainNIF=resp["mainNIF"],
+        stateRegister=resp["stateRegister"],
+        zipCode=resp["zipCode"],
+        streetType=resp["streetType"],
+        streetName=resp["streetName"],
+        number=resp["number"],
+        districtType=resp["districtType"],
+        district=resp["district"],
+        stateCode=resp["stateCode"],
+        cityInternalId=resp["cityInternalId"],
+        phoneNumber=resp["phoneNumber"],
+        email=resp["email"],
+        contributor=resp["contributor"]
+    )
+
+except Exception as e:
+    print(f"exception: {e}")
