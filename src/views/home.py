02f1366fc4a266093fe6e72a648713c28e7edef4
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-22 16:59:21 -0300

    Initialize codcoligada and customers_vendors attributes in HomeView class

--- src/views/home.py
+++ src/views/home.py
@@ -5,6 +5,8 @@ from utils.ui import show_message
 class HomeView:
     def __init__(self, page: ft.Page):
         self.page = page
+        self.codcoligada = str()
+        self.customers_vendors = dict()
     
     def show(self):
         def start_automation(e):
